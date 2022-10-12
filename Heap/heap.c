e59dcebc594ac3768499abb2a583fa9fb14748cd
Carter Morgan <carter.r.morgan@gmail.com>
2022-10-11 20:52:54 -0600

    Implement alloc_heap

---
+++ Heap/heap.c
@@ -0,0 +1,82 @@
+/*
+ *
+ *
+ */
+
+#include <stdlib.h>
+#include "heap.h"
+
+const int DEFAULT_SIZE = 64;
+const int RESIZE_FACTOR = 2;
+const int BRANCH_FACTOR = 4;
+
+struct heap_t {
+  int size;
+  int buffer_size;
+  void **data;
+  comparator compare;
+};
+
+void heapify(heap_t *heap) {
+
+}
+
+/***************************
+ * ALLOCATORS/DEALLOCATORS *
+ ***************************/
+
+/*
+ * Allocate a new heap.
+ *
+ * Input:
+ *   - compare: function used to compare items within the heap.
+ *   - data: (optional) Pointer to an existing buffer. The data in that buffer
+ *       is used to initialize the heap & the heap takes ownership over the
+ *       buffer. If not null, the size of the buffer must also be given.
+ *   - size: (optional) If an existing buffer is given, the size of that buffer.
+ *       else, the size to initialize the heap's buffer with.
+ */
+heap_t *alloc_heap(comparator compare, void **data, int size) {
+  if (!compare) return NULL;
+
+  heap_t *heap = (heap_t*)malloc(sizeof(heap_t));
+  *heap = (heap_t){size, size, data, compare};
+
+  if (data == NULL) {
+    heap->size = 0;
+    heap->buffer_size = size ? size : DEFAULT_SIZE;
+    heap->data = malloc(heap->buffer_size * sizeof(void*));
+  }
+  else {
+    if (size <= 0) return NULL;
+    heapify(heap);
+  }
+
+  return heap;
+}
+
+/*
+ * Free the given heap
+ */
+void free_heap(heap_t *heap) {
+
+}
+
+/********************
+ * MEMBER FUNCTIONS *
+ ********************/
+
+/*
+ * Return a pointer to the smallest item in the heap w/o removing it.
+ */
+void *peek_min(heap_t *heap);
+
+/*
+ * Return a pointer to the smallest item in the heap and remove it.
+ */
+void *pop_min(heap_t *heap);
+
+/*
+ * Add the given item to the heap.
+ */
+void push(heap_t *heap, const void *item);
